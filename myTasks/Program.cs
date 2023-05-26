d91a394f14bf8e3452bc2f5e88546697013c5e07
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-26 20:33:34 +0500

    1st commit: first task

--- myTasks/Program.cs
+++ myTasks/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ЗАДАЧА 1 (41). Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int countNumbers = 0;
+int countEvenNumbers = 0;
+string quit = "processing";
+while (quit.Equals("processing"))
+{
+    int userNumber = GetUserInput("число");
+    countNumbers++;
+    if (userNumber > 0 ) countEvenNumbers++;
+    Console.WriteLine($"Введённых чисел: {countNumbers}; положительных: {countEvenNumbers}.");
+    int exit = GetUserInput("0 для завершения");
+    if (exit == 0) quit = "stop!";
+}
+Console.WriteLine($"Введённых чисел: {countNumbers}; положительных: {countEvenNumbers}.");
+
+int GetUserInput(string name)
+{
+    Console.WriteLine();
+    Console.Write($"Введите {name} : ");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    return userInput;
+}
+// ***************************************************************************************************************************************
